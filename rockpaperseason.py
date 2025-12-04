95d5086212227911a0b59be5f1afa1d21b5118a7
shubham-nov06 <dograshubham06@gmail.com>
2025-12-04 20:52:57 +0530

    Updated changes

--- rockpaperseason.py
+++ rockpaperseason.py
@@ -3,4 +3,5 @@ options = [ "Rock", "Paper", "Scissors"]
 while True: 
     user = input("Input Rock/Paper/Scissors")
     computer = random.choice(options)
-    print(computer)
+    print("Computer",computer)
+    
