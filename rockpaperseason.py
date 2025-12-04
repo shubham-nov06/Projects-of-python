59fcbe8acfb21c7cc2cadbfd1e809d7f8e42678a
shubham-nov06 <dograshubham06@gmail.com>
2025-12-04 21:01:57 +0530

    Updated changes

--- rockpaperseason.py
+++ rockpaperseason.py
@@ -7,7 +7,7 @@ while True:
     
     if user==computer:
         print("Draw")
-    elif (user=="Rock" and computer=="Secssors") or \
+    elif (user=="Rock" and computer=="Scissors") or \
     (user== "Paper" and computer=="Rock") or \
     (user == "Scissors" and computer=="Paper"):
         print("You won the game bro")
