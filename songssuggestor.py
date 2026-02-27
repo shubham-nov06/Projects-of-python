81c437d9745f2c225f75e360bcba22578fd4f365
shubham-nov06 <dograshubham06@gmail.com>
2026-02-27 18:36:39 +0000

    Updated changes

--- songssuggestor.py
+++ songssuggestor.py
@@ -2,5 +2,5 @@ mood_songs = {
     "Happy" : [" For a reason " , "  Tere liye " ,  "Doron Doorn"],
     "Gym" : ["Sheesha" , "2 Gaz Ka Ghoo" , "Putt Jat da"],
     "Romantic" : ["Maula mere maula" , "Naam Tera "] ,
-    "Sad" : ["Unforgettablee"]
+    "Sad" : ["Unforgettable", ]
 }
