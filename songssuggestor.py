83cd1ae502319a966469da012f19137ff11c8db7
shubham-nov06 <dograshubham06@gmail.com>
2026-02-27 18:36:39 +0000

    Updated changes

--- songssuggestor.py
+++ songssuggestor.py
@@ -10,7 +10,7 @@ mood = str(input("Enter your mood = "))
 if mood in mood_songs :
     print("Recommended songs are = ")
     for song in mood_songs[mood] :
-        for i in range (3 - 1):
+        for i in range (3 ):
          print(i , song )
     
 else: print("Not a valid input ")
