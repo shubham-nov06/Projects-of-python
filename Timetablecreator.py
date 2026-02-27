20b094fc35884bdfc6c4ed7dc2ed5f0328b5f8f5
shubham-nov06 <dograshubham06@gmail.com>
2026-02-27 20:15:55 +0000

    Updated changes

--- Timetablecreator.py
+++ Timetablecreator.py
@@ -9,10 +9,13 @@ total_hours = float(input("Enter the number of hours you have "))
 
 subject_list = subject.split(",")
 
+
 hours_per_subject = total_hours / len(subject_list)
+
 print("\n Your study plan is = \n ")
+
 for subject in subject_list:
-    print(subject.strip(), " ->  ", round(hours_per_suject), 2, "hours"s)
+    print(subject.strip(), " ->  ", round(hours_per_suject), 2, "hours")
 
 
 
