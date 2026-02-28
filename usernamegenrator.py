bc39adeaa6a9344fdc0120612440fd9f486e4a54
shubham-nov06 <dograshubham06@gmail.com>
2026-02-28 18:27:31 +0530

    Updated changes

--- usernamegenrator.py
+++ usernamegenrator.py
@@ -1,6 +1,6 @@
 import random
 
 name = input("Enter your name ").lower()
-number = random(1, 99)
+number = random.randint(1, 99)
 username = name + str(number)
 print("Your username is :" ,username)
