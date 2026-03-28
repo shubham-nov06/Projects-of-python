28f9d483aec576080aebc50b43cc979842c62124
shubham-nov06 <dograshubham06@gmail.com>
2026-03-28 16:38:19 +0000

    2026-03-28 22:08:19

---
+++ generic.java
@@ -0,0 +1,15 @@
+class Box<T> {
+    T value;
+}
+
+    void setVlaue(T value) {
+        This.value = value;
+    }
+
+T get
+
+    value() {
+    Return value;
+}
+
+public static void main (Stiring [] args )
